7cadf99b2fc594f2a0bda99fddbc60a3c9068a3c
Danielateresa <dany.t_87@hotmail.it>
2022-11-09 14:35:18 +0100

    Added jumbotron

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from "./components/AppTitle.vue"
+import AppHero from "./components/AppHero.vue"
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 
@@ -12,6 +14,8 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero />
+
 </template>
 
 
---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <section class="jumbotron">
+        <h3>Il mio primo jumbotron in Vite</h3>
+        <img src="https://picsum.photos/1000/400" alt="">
+    </section>
+
+</template>
+
+<style scoped>
+.jumbotron {
+    position: relative;
+}
+
+h3 {
+    color: white;
+    position: absolute;
+    top: 80%;
+    left: 20px;
+}
+</style>

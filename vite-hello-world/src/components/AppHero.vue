7dad50a6c1293987a9dedd84c2b129a9a27460ab
Danielateresa <dany.t_87@hotmail.it>
2022-11-09 14:43:46 +0100

    Added more style on AppHero

--- vite-hello-world/src/components/AppHero.vue
+++ vite-hello-world/src/components/AppHero.vue
@@ -1,8 +1,16 @@
 <template>
 
     <section class="jumbotron">
-        <h3>Il mio primo jumbotron in Vite</h3>
+
         <img src="https://picsum.photos/1000/400" alt="">
+        <div class="jumbo_text">
+            <h3>Il mio primo jumbotron in Vite</h3>
+            <div class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolore unde nihil eum porro iste, totam
+                quis deserunt qui suscipit numquam velit nemo autem odio. Illo inventore labore cumque perferendis.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, voluptates?
+            </div>
+        </div>
     </section>
 
 </template>
@@ -12,10 +20,14 @@
     position: relative;
 }
 
-h3 {
+.jumbo_text {
+    width: 40%;
+    text-align: left;
     color: white;
+    background-color: rgba(57, 57, 240, 0.446);
+    padding: 2rem;
     position: absolute;
-    top: 80%;
+    top: 20%;
     left: 20px;
 }
 </style>
